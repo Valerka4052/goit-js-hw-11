0f9bae3669f8979e6e1f5611940a89a911cf089b
Valerka4052 <114054150+Valerka4052@users.noreply.github.com>
2023-01-30 19:32:50 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -20,24 +20,4 @@ export async function fetchItems(searchItems, numOfPage) {
     console.error(error);
   };
 };
-// export async function fetchItems(searchItems, numOfPage) {
-//   try {
-//     const options = {
-//       baseURL: 'https://pixabay.com/api/',
-//       params: {
-//         key: '33181746-c383b02523a4c167923538d20',
-//         q: `${searchItems}`,
-//         image_type: "photo",
-//         orientation: "horisontal",
-//         safesearch: true,
-//         per_page: 20,
-//         page: numOfPage,
-//       },
-//     };
-//     const response = await axios(options);
-//     const arrayThumbs = response.data.hits;
-//     return arrayThumbs
-//   } catch (error) {
-//     console.error(error);
-//   };
-// };
+
