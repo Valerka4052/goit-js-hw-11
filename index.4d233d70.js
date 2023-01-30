7415c0c4e372a64e99b4218e8b0d2f2caca49d8e
Valerka4052 <114054150+Valerka4052@users.noreply.github.com>
2023-01-30 17:24:57 +0000

    Deploying to gh-pages from @ Valerka4052/goit-js-hw-11@2c356b8645d6fedf87f16333321ddf2a5ef7f840 🚀

--- index.4d233d70.js
+++
@@ -1,2 +0,0 @@
-console.log("hello"),console.log("sdfwiyfgier");
-//# sourceMappingURL=index.4d233d70.js.map
--- index.b13e68c0.js
+++
@@ -1,2 +0,0 @@
-console.log("hello"),console.log("sdfwiyfgier");
-//# sourceMappingURL=index.b13e68c0.js.map
